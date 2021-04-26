6a828342b89044b382f0a303b5f3876ff95c9c73
Austin FitzGerald <fitzgerald.austind@gmail.com>
2021-04-26 13:39:54 -0500

    Added a simple way to test retrieving clients, have not tested yet though.

--- Core/Src/access_point.c
+++ Core/Src/access_point.c
@@ -21,9 +21,9 @@ bool createAP(void)
 //TODO [@fitzgeralaus] need to test
 /**
  * @brief Finds all clients connected to the AP, stores in APClients variable.
- * @retval Success of operation.
+ * @retval Operation success
  */
-bool getClientRSSIs(void)
+bool getClients(void)
 {
 	return (WIFI_ListAPClients(APClients) == WIFI_STATUS_OK);
 }
