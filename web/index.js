df5ac500c4fce5173ab5a5806885580dff64037b
Austin FitzGerald <fitzgerald.austind@gmail.com>
2021-05-06 21:43:49 -0500

    Controller now snaps back to middle.

--- web/index.js
+++ web/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const http = require('http');
 const net = require('net');
-const { Server } = require("socket.io");
+const {
+  Server
+} = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,13 +12,13 @@ const tcpSocket = new net.Socket();
 
 // Set up routes for resources
 app.get('/scripts/socket.io.min.js', function(req, res) {
-    res.sendFile(__dirname + '/node_modules/socket.io/client-dist/socket.io.min.js');
+  res.sendFile(__dirname + '/node_modules/socket.io/client-dist/socket.io.min.js');
 });
 app.get('/scripts/jquery.min.js', function(req, res) {
-    res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.min.js');
+  res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.min.js');
 });
 app.get('/scripts/jquery-ui.min.js', function(req, res) {
-    res.sendFile(__dirname + '/node_modules/jquery-ui-dist/jquery-ui.min.js');
+  res.sendFile(__dirname + '/node_modules/jquery-ui-dist/jquery-ui.min.js');
 });
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -32,31 +34,29 @@ server.listen(80, () => {
   console.log('listening on *:80');
 });
 
-var x = 0.5, y = 0.5;
+var x = 0.5,
+  y = 0.5;
 
 io.on('connection', (webSocket) => {
-	console.log('Web client connected.');
-	
-	tcpSocket.connect(8080, '192.168.10.1', function() {
-		tcpSocket.setEncoding("utf8");
-		console.log('Connected to TCP server.');
-		setInterval(sendPosition, 500);
-	});
+  console.log('Web client connected.');
 
-	
-	tcpSocket.on('data', function(recData) {
-		io.emit('inputIn', recData.split(','));
-		//console.log(recData.split(','));
-	});
-	
-	webSocket.on('inputOut', ([i_x,i_y]) => {
-		x = i_x;
-		y = i_y;
-	});
+  tcpSocket.connect(8080, '192.168.10.1', function() {
+    tcpSocket.setEncoding("utf8");
+    console.log('Connected to TCP server.');
+    setInterval(sendPosition, 500);
+  });
+
+  tcpSocket.on('data', function(recData) {
+    io.emit('inputIn', recData.split(','));
+  });
+
+  webSocket.on('inputOut', ([i_x, i_y]) => {
+    x = i_x;
+    y = i_y;
+  });
 });
 
 // on timer
 function sendPosition() {
-	tcpSocket.write(x + ',' + y);
+  tcpSocket.write(x + ',' + y);
 }
-
--- web/slider.js
+++ web/slider.js
@@ -1,13 +1,11 @@
 // https://codepen.io/tyler-murphy/pen/tHsAu
 var slider = {
-	
-	
-  
+
   get_position: function() {
     var marker_pos = $('#marker').position();
     var left_pos = marker_pos.left + slider.marker_size / 2;
     var top_pos = marker_pos.top + slider.marker_size / 2;
-    
+
     slider.position = {
       left: left_pos,
       top: top_pos,
@@ -15,9 +13,9 @@ var slider = {
       y: Math.round((slider.round_factor.y * (slider.height - top_pos) * slider.ymax / slider.height)) / slider.round_factor.y,
     };
   },
-  
+
   draw: function(x_size, y_size, xmax, ymax, marker_size, round_to) {
-    
+
     slider.marker_size = marker_size;
     slider.height = y_size;
     slider.width = x_size;
@@ -28,7 +26,7 @@ var slider = {
       x: round_to,
       y: round_to,
     };
-    
+
     $("#markerbounds").css({
       "width": (x_size + marker_size).toString() + 'px',
       "height": (y_size + marker_size).toString() + 'px',
@@ -42,11 +40,11 @@ var slider = {
     $("#marker").css({
       "width": marker_size.toString() + 'px',
       "height": marker_size.toString() + 'px',
-	  "inset": (y_size - marker_size).toString() / 2 + 'px ' + 'auto auto ' + (x_size - marker_size).toString() / 2 + 'px'
+      "inset": (y_size - marker_size).toString() / 2 + 'px ' + 'auto auto ' + (x_size - marker_size).toString() / 2 + 'px'
     });
     $("#widget").css({
       "width": (x_size + marker_size).toString() + 'px',
     });
-    
+
   },
 };
